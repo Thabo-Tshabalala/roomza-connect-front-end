73f3021d64ecf3719ffce10594b169edfe98cd5d
Thabo-Tshabalala <justicethabo21@gmail.com>
2025-03-19 21:31:26 +0200

    Cleanup : Modularizing Code

--- roomza-connect/src/app/app.routes.ts
+++ roomza-connect/src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { LandingPageComponent } from './landing-page/landing-page.component';
+import { NavigationBarComponent } from './navigation-bar/navigation-bar.component';
 
 export const routes: Routes = [
-    {path:'',component: LandingPageComponent}
+    {path:'',component: LandingPageComponent},
+    {path:'',component:NavigationBarComponent}
 ];
