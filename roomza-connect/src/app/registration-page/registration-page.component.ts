57d27a792b639de40a168d800e87849e56521924
Thabo-Tshabalala <justicethabo21@gmail.com>
2025-05-19 23:55:06 +0200

    registration page prep

--- roomza-connect/src/app/registration-page/registration-page.component.ts
+++ roomza-connect/src/app/registration-page/registration-page.component.ts
@@ -1,11 +1,8 @@
 import { Component } from '@angular/core';
-
 @Component({
   selector: 'app-registration-page',
-  imports: [],
   templateUrl: './registration-page.component.html',
-  styleUrl: './registration-page.component.css'
+  styleUrls: ['./registration-page.component.scss']
 })
 export class RegistrationPageComponent {
-
 }
